44d6eb5ea38caeea8149800c2c4bae058c4bf546
bgbblz10 <bgbblz10@gmail.com>
2025-02-10 18:09:30 +0200

    Traduction de localite

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import messages_fr from "./translations/fr.json";
+
+const DEFAULT_CONFIG = {
+  "translations": [{ key: "fr", messages: messages_fr }],
+}
+
+export const LanguageFrModule = (cfg) => {
+  return { ...DEFAULT_CONFIG, ...cfg };
+}
